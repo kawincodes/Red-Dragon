9efd8370dbd7d5f8e2df574d97e2d08b52f8c03f
Ayushdeveloper001 <117559953+Ayushdeveloper001@users.noreply.github.com>
2022-11-06 12:00:06 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,11 +29,11 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.vcardowner = ['+918602306506'] // owner number
+global.ownername = "Ayush/Danyon❯❯" // owner name
 global.ytname = "" // yt chanel name
-global.socialm = "GitHub: prabhasha2006" // github or insta name
-global.location = "India, Mizoram, Aizawl" // location
+global.socialm = "insta:https://www.instagram.com/itzmathis_god/" // github or insta name
+global.location = "India, Madhya Pradesh" // location
 
 //bot body 
 global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
@@ -47,16 +47,16 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
+global.owner = ['𝔸𝕪𝕦𝕤𝕙 - 𝔹𝕠𝕥']
+global.ownertag = '+918602306506' //your tag number
+global.botname = '𝔸𝕪𝕦𝕤𝕙 - 𝔹𝕠𝕥' //ur bot name
 global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.author = "𝔸𝕪𝕦𝕤𝕙 - 𝔹𝕠𝕥" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[ ◉ 𝔸𝕪𝕦𝕤𝕙 - 𝔹𝕠𝕥 ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+918602306506'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
